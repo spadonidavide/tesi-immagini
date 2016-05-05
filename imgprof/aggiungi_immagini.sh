a3072f056100b9cd5c70949c88ff63cadbc4208b
davide <spadonidavide@gmail.com>
2016-05-05 11:41:29 +0200

    aggiunto il dump del db

--- imgprof/aggiungi_immagini.sh
+++ imgprof/aggiungi_immagini.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 rm path_immagini_ind.txt;
-for f in immagini/*.jpg
+for f in /home/davide/Immagini/immagini/*.jpg
 do
 	echo $f >> path_immagini_ind.txt;
 done
 
 ./a.out;
-for f in immagini/*.jpg
+for f in /home/davide/Immagini/immagini/*.jpg
 do
 	mv $f /home/davide/Immagini/immagini/immagini_caricate;
 done
